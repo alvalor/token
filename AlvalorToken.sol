b188d536661528b3fafdf2521e7dbac7aad6251a
Max Wolter <awishformore@gmail.com>
2017-11-10 14:56:08 +0100

    changed pure back to constant

--- AlvalorToken.sol
+++ AlvalorToken.sol
@@ -6,25 +6,25 @@ pragma solidity ^0.4.15;
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
     // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
@@ -38,19 +38,19 @@ library SafeMath {
  */
 
 library Math {
-  function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
     return a >= b ? a : b;
   }
 
-  function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
     return a < b ? a : b;
   }
 
-  function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
     return a >= b ? a : b;
   }
 
-  function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
     return a < b ? a : b;
   }
 }
